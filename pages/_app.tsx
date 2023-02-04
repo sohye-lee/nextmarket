97eeb441aad4b16f55b8e277b795498afa7762c4
sohye-lee <sohyelee@gmail.com>
2023-02-04 00:00:02 -0500

    header footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,26 @@ import { Provider } from 'react-redux';
 import store from '../store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
+import Head from 'next/head';
 let persistor = persistStore(store);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />
-      </PersistGate>
-    </Provider>
+    <>
+      <Head>
+        <title>Kloe</title>
+        <meta
+          name="description"
+          content="Eco friendly market for all used goods"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
