6ce3e4686163f8b9067f3994f6765af87353d2ac
sohye-lee <sohyelee@gmail.com>
2023-02-08 07:14:24 -0500

    top

--- components/header/top.tsx
+++ components/header/top.tsx
@@ -1,17 +1,29 @@
 import Link from 'next/link';
 import styles from './styles.module.scss';
+import { IoIosSearch } from 'react-icons/io';
 
 export default function Top() {
   return (
     <div className={styles.top}>
       <div className={styles.top_container}>
-        <div>
+        <div className={styles.logo}>
           <Link href="/">
             <img src="/logo.svg" width="80" alt="logo" />
           </Link>
         </div>
-        <input type="search" name="search" />
-        <div className={styles.top__list}></div>
+        <div className={styles.input__group}>
+          <input type="search" name="search" />
+          <button type="submit">
+            <IoIosSearch name="search" width={80} height={80} size={24} />
+          </button>
+        </div>
+        <div className={styles.top__list}>
+          <Link href="">Sell on Kloe</Link>
+          <Link href="">Sign In</Link>
+          <Link href="/register" className={styles.top__button}>
+            Sign Up to Shop
+          </Link>
+        </div>
       </div>
     </div>
   );
